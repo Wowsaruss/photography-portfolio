277c421e326e8f3d1588725c77e2bfb6a95db94e
Wowsaruss <russell.hayes.dev@gmail.com>
2019-04-03 07:27:38 -0700

    Refactoring to photo portfolio

--- src/colors.js
+++ src/colors.js
@@ -1,5 +1,6 @@
+export const Black = '#000000';
+export const White = '#ffffff';
 export const LightRed = '#e04343';
-// export const DarkBlue = '#030a19';
 export const DarkBlue = '#022230';
 export const Blue = '#2f7eed';
 export const LightGray = '#dcdee2';
